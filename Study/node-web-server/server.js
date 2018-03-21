fd56e48cd0acbcfd999efb49939daf8a411f33d0
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-03-21 09:35:55 +0900

    At the end of Module 43
    Handlebar as Express.js view engine

--- Study/node-web-server/server.js
+++ Study/node-web-server/server.js
@@ -1,23 +1,35 @@
 const express = require('express');
 
+// Express.js view engine for handlebar.js
+const hbs = require('hbs');
+
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get( '/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'Hiroshi',
-        likes: [ 
-            'Drinking',
-            'Reading',
-            'Study'
-        ]
+    // res.send({
+    //     name: 'Hiroshi',
+    //     likes: [ 
+    //         'Drinking',
+    //         'Reading',
+    //         'Study'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page', 
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my home'
     });
 });
 
 app.get('/about', (req,res) => { 
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About This Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req,res) => {
