94f8174d6edc056173257d27a2f460c4e2ddbd5c
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-03-21 10:29:53 +0900

    At the end of module 45
    Express middleware

--- Study/node-web-server/server.js
+++ Study/node-web-server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 
 // Express.js view engine for handlebar.js
@@ -19,9 +20,26 @@ hbs.registerHelper('screamIt', (text) => {
 
 app.set('view engine', 'hbs');
 
-// Static files
-app.use(express.static(__dirname + '/public'));
+// other middleware - plugin to request processing
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) { 
+            console.log('Unable to append to server.log');
+        }
+    });
+    next(); // go to next step
+});
 
+// == When used, always show maintenance.hbs, beause next() is not called
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
+
+// Static files (after maintenance)
+app.use(express.static(__dirname + '/public'));
 
 app.get( '/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
