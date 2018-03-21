64c51a77310dde27b62d6c40a83e6128b37c7a0c
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-03-21 09:11:00 +0900

    At the end of module 42
    With static HTML files in public folder

--- Study/node-web-server/server.js
+++ Study/node-web-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get( '/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
     res.send({
@@ -23,5 +25,7 @@ app.get('/bad', (req,res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-app.listen(3000);
+app.listen(3000 ,()=> {
+    console.log('Server is up on port 3000');
+});
 
