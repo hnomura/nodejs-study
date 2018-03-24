fdade64e1e553c3428ab538d5d19a66bc8aa79ff
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-03-24 18:27:59 +0900

    At the end of module 73
    First REST with post

--- Study/node-todo-api/server/server.js
+++ Study/node-todo-api/server/server.js
@@ -1,72 +1,28 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// use built-in default promise
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} =  require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// model 
-var Todo = mongoose.model('Todo', {
-    text: { 
-        type: String, 
-        required: true,
-        minLength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }, 
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express(); 
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then( (doc)=> {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-// // new instance 
-// var newTodo = new Todo({
-//     text: 'Eat Lunch'
-// });
 
-// // save 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save tood');
-// });
-
-// // new instance 
-// var otherToDo = new Todo({
-//     text: 'Take a rest', 
-//     completed: true, 
-//     completedAt: new Date().getDate()
-// });
-
-// // save 
-// otherToDo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save tood', e);
-// });
-
-// User model 
-// email = required, with trim - strin gtype - set min length = 1
-var User = mongoose.model('User', {
-    email: {
-        type:   String, 
-        required: true, 
-        minLength: 1, 
-        trim: true
-    }
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
 
-var newUser = new User({
-    email: 'hnomura.babou@gmail.com'
-});
-
-// save 
-newUser.save().then((doc) => {
-    console.log('Saved user', doc);
-}, (e) => {
-    console.log('Unable to save user', e);
-});
 
