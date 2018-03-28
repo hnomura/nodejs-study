65cd1b19f1a252cbe38eb61fd73b34367d011027
hnomura@slb.com <hnomura@slb.com>
2018-03-28 16:10:03 +0900

    At the end of module 90
    Add instance methods to user model.
    Generate token when posting new data with JWT
    Return token in 'x-auth' property of response header.
    Return only email and _id properties in response body.

--- Study/node-todo-api/server/models/user.js
+++ Study/node-todo-api/server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type:   String, 
         required: true, 
@@ -32,5 +34,35 @@ var User = mongoose.model('User', {
         }
     }]
 });
+
+
+// Define instance methods
+// cannot be arrow function because we use 'this'
+
+// Overwrite existing instance method 
+UserSchema.methods.toJSON = function () {
+    var user = this; 
+    var userObject = user.toObject(); // mongoose document object to JSON object (attributes only)
+    return _.pick(userObject, ['_id', 'email']); // extract only _id and email attributes 
+};
+
+// Add New instance method 
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+
+    // JWT of {_id:user_id, access:'auth'} with 'abc123' as secret 
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+//  user.tokens.push({access,token});
+    user.tokens = user.tokens.concat( [{access,token}] );
+
+    // NOTE: tricky part. Return promise, and pass token to the callback function to the returned promise..
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = { User }; 
