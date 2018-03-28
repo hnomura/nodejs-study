d5b661ef7efe50e1e0aeedf76abf4aa79247dd41
hnomura@slb.com <hnomura@slb.com>
2018-03-28 17:56:55 +0900

    At the end of module 92
    Hashing user password when saving to db.

--- Study/node-todo-api/playground/hashing.js
+++ Study/node-todo-api/playground/hashing.js
@@ -1,4 +1,7 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 /// // == SHA256 cryptoing 
 // var message = 'I am user number 3';
@@ -29,19 +32,32 @@ const {SHA256} = require('crypto-js');
 //     console.log('Data was changed. Do not trust!');
 // }
 
-const jwt = require('jsonwebtoken');
-// jwt.sign (takes object, and signs it)
-// jwt.verify 
+// // jwt.sign (takes object, and signs it)
+// // jwt.verify 
 
-var data = { 
-    id: 10
-};
+// var data = { 
+//     id: 10
+// };
 
-var token = jwt.sign(data, 'secret123');
-console.log(token); 
+// var token = jwt.sign(data, 'secret123');
+// console.log(token); 
 
-var decoded = jwt.verify(token, 'secret123');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, 'secret123');
+// console.log('decoded', decoded);
 
+var password = '123abc!';
 
+// number of rounds = 10 in this case
+bcrypt.genSalt( 10, (err,salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+// hash once generated
+var hashedPassword = '$2a$10$FO7Nc4yvP3uV0KLXAgjEBOg0rRWnuQr8pvEkCCKdZHVFzs31XXMvG';
+
+bcrypt.compare( password, hashedPassword, (err,res) => {
+    console.log(res);
+});
 
