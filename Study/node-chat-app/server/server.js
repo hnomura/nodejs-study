95e62a78567d61a2aac71ca3e29181b9b782bf7f
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-04-02 22:23:37 +0900

    At the end of module 108
    Custom event emitting and listening.

--- Study/node-chat-app/server/server.js
+++ Study/node-chat-app/server/server.js
@@ -15,6 +15,16 @@ app.use(express.static( publicPath ));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'hnomura',
+        text: 'New Message', 
+        createdAt: 234
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
