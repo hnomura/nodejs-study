bf695f6312baab03af87311493145fb73d9d49bf
Hiroshi Nomura <hnomura.babou@gmail.com>
2018-04-06 21:16:50 +0900

    Att the end of module 118
    Format timestamps using moment.js

--- Study/node-chat-app/playground/time.js
+++ Study/node-chat-app/playground/time.js
@@ -2,9 +2,12 @@
 
 var moment = require('moment');
 
-// var date = new Date();
+var createdAt = new Date();
 // console.log(date.getMonth());
 
-var date = moment();
+var date = moment(createdAt);
 // Apr 6th 2018 8:55:18 pm
 console.log(date.format('MMM Do YYYY h:mm:ss a'));
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
