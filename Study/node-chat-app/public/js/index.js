7e04b05031b3b23505044cccfe8073116aee1e92
hnomura@slb.com <hnomura@slb.com>
2018-04-06 14:47:34 +0900

    At the end of module 113
    Add form for message.
    Add ordered list for incoming msgs.
    Use jQuery in index.js
    jQuery source downloaded

--- Study/node-chat-app/public/js/index.js
+++ Study/node-chat-app/public/js/index.js
@@ -10,12 +10,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Auto Generator', 
-    text: 'Client Connected'
-}, function (data) {
-    // acknowledgement
-    console.log('Got it', data);
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault(); // prevent page refresh defaul tbehavior
+
+    socket.emit('createMessage', {
+        from: 'User', 
+        text: jQuery('[name=message]').val()
+    }, function () {
+        // acknowledgement callback 
+    });
 });
