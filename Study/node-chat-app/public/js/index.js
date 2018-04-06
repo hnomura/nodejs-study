a3d55ae5ab886f85350e7f3c4d0ece83e2504570
hnomura@slb.com <hnomura@slb.com>
2018-04-06 13:34:57 +0900

    At the end of module 110
    socket.emit() and socket.broadcast.emit().
    Note difference from io.emit().

--- Study/node-chat-app/public/js/index.js
+++ Study/node-chat-app/public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'xxxx@eample.com',
-        text: 'CreateMessage'
-    });
 });
 
 socket.on('disconnect', function () {
--- Study/node-chat-app/server/server.js
+++ Study/node-chat-app/server/server.js
@@ -15,15 +15,39 @@ app.use(express.static( publicPath ));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // Welcome message to newly connected socket
     socket.emit('newMessage', {
-        from: 'hnomura',
-        text: 'New Message', 
-        createdAt: 234
-    })
+        from: 'Admin', 
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
 
+    // Notification to everyone except for the newly connected socket 
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin', 
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        // To everyone including the socket sending this message
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+        // // everybody but this socket 
+        // socket.broadcast.emit('newMessage', {
+        // from: message.from, 
+        // text: message.text, 
+        // createdAt: new Date().getTime()
+        // });
+      
     });
+
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
